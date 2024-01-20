d6d4dee438b155d8cdcd6962d269e242f9252b05
mochrira <mochrira@gmail.com>
2024-01-20 16:26:28 +0700

    change submitted form method

--- www/update.php
+++ www/update.php
@@ -13,7 +13,7 @@ shell_exec("git fetch --tags");
 $latestTag = shell_exec("git describe --tags \"$(git rev-list --tags --max-count=1)\"");
 ?>
 
-<form action="update.php">
+<form action="update.php" method="POST">
     <input type="hidden" name="tag" value="<?php echo $latestTag; ?>">
     <h1>You are about to update app to version : <?php echo $latestTag; ?></h1>
     <button type="submit">Update Now</button>
