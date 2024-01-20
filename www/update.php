e81d4a7f0d8d6781e96f615a87e39ea141d79b57
mochrira <mochrira@gmail.com>
2024-01-20 16:25:30 +0700

    change update script

--- www/update.php
+++ www/update.php
@@ -1,1 +1,20 @@
-<?php echo shell_exec(__DIR__."/../update.sh");
+<?php 
+
+chdir(__DIR__.'/../');
+
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == 'POST') {
+    $tag = $_POST['tag'];
+    echo shell_exec("git checkout ".$tag);
+    die();
+}
+
+shell_exec("git fetch --tags");
+$latestTag = shell_exec("git describe --tags \"$(git rev-list --tags --max-count=1)\"");
+?>
+
+<form action="update.php">
+    <input type="hidden" name="tag" value="<?php echo $latestTag; ?>">
+    <h1>You are about to update app to version : <?php echo $latestTag; ?></h1>
+    <button type="submit">Update Now</button>
+</form>
